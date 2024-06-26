2f71023e51f77c9a7d38ba4f5c7973f51e4a5bc8
Hakan Aydoğmuş <144350544+hakanaydogmus2@users.noreply.github.com>
2024-06-26 09:48:48 +0300

    solved 2 problem

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int seaLevel = 0;
+        int counter = 0;
+        foreach(var s in path){
+            if(s == 'U'){
+                seaLevel++;
+                
+                if(seaLevel == 0){
+                    counter++;
+                }
+            }
+            else {
+                seaLevel--;
+                
+            }
+        }
+        return counter;        
+
+    }
+
+}
---
+++ SaleByMatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static int sockMerchant(int n, List<int> ar)
+    {   
+        var sockcount = 0;
+        var a = new List<int>();
+        foreach(var i in ar){
+            
+            if(!a.Contains(i)){
+                a.Add(i);
+            }
+            else{
+                sockcount++;
+                a.Remove(i);
+            }
+        }
+        return sockcount;
+    }
+
+}
